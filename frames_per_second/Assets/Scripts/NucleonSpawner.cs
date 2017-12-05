96dc24156fc15a48c736a9651a8854f471e5176d
Leif Hetland <lehetland1@gmail.com>
2017-12-05 13:56:36 -0600

    script picks a random prefab, instantiates it, places it at a random distance from our game object and draws in the prefabs to the center

---
+++ frames_per_second/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+	public float timeBetweenSpawns;
+	public float spawnDistance;
+	public float timeSinceLastSpawn;
+	public Nucleon[] nucleonPrefabs;
+
+	void SpawnNucleon() {
+		Nucleon prefab = nucleonPrefabs [Random.Range (0, nucleonPrefabs.Length)];
+		Nucleon spawn = Instantiate<Nucleon>(prefab);
+		spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+	}
+
+
+
+	void FixedUpdate() {
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn -= timeBetweenSpawns;
+			SpawnNucleon();
+		}
+	}
+}
